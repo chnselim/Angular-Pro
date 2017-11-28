22eefa810fad6850f6126e3a46dc138457ffd4ed
chnselim <chnselim@outlook.com>
2017-11-28 09:05:01 +0300

    MOVE changeSortType from smart table to quick table

--- components/quick-table/quick-table.component.ts
+++ components/quick-table/quick-table.component.ts
@@ -1,8 +1,11 @@
-import {AfterViewInit, Component, ContentChildren, EventEmitter, Input, Output, QueryList} from '@angular/core';
+import {
+    AfterViewInit, Component, ContentChildren, EventEmitter, Input, Output, QueryList
+} from '@angular/core';
 import {QuickTableColumnDirective} from './quick-table-column.directive';
 import {ComponentBase} from '../base.component';
 import {isNullOrUndefined} from "util";
 import {QuickSelectItemDirective} from "bng-angular-base/components/quick-select/quick-select-item.directive";
+import {el} from "@angular/platform-browser/testing/src/browser_util";
 
 @Component({
     selector: 'quick-table',
@@ -43,8 +46,13 @@ export class QuickTableComponent extends ComponentBase implements AfterViewInit 
 
     public index_list = [];
 
+    public sort_by: string;
+
+    public is_column_sort_by_descending: boolean;
+
     public get renderFilter(): boolean {
         return this.columns.some((column) => {
+
             return column.filter_template != null;
         });
     }
@@ -62,7 +70,19 @@ export class QuickTableComponent extends ComponentBase implements AfterViewInit 
     public changePerPage(selected_item: QuickSelectItemDirective) {
         const query_value = selected_item.value;
         this.per_page = parseInt(query_value);
+        this.current_page = 1;
         this.getIndexNumberList(this.current_page);
+    }
+
+    public changeSortType(column) {
+        this.sort_by = column.sort_by;
+        if (column.is_column_sort_icon_descending) {
+            this.is_column_sort_by_descending = false;
+            column.is_column_sort_icon_descending = false;
+        } else {
+            this.is_column_sort_by_descending = true;
+            column.is_column_sort_icon_descending = true;
+        }
     }
 
     public getIndexNumberList(current_page: number) {
