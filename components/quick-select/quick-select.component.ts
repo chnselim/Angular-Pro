8379a2c23112fc14ac9f71124ea44b9a0f8459f8
chnselim <chnselim@outlook.com>
2018-01-19 13:09:04 +0300

    ADD afterViewInit to update if selected field exist. used setTimeout(0) to block 'Expression has changed after it was checked' warning of Angular.

--- components/quick-select/quick-select.component.ts
+++ components/quick-select/quick-select.component.ts
@@ -1,67 +1,3 @@
-// import {AfterViewInit, Component, ContentChildren, EventEmitter, Input, OnInit, Output, QueryList} from '@angular/core';
-// import {ComponentBase} from '../base.component';
-// import {QuickSelectItemDirective} from './quick-select-item.directive';
-// import {QuickTableComponent} from '../quick-table/quick-table.component';
-// import {el} from "@angular/platform-browser/testing/src/browser_util";
-//
-// @Component({
-//     selector: 'quick-select',
-//     templateUrl: 'quick-select.component.html',
-//     providers: [
-//         {
-//             provide: ComponentBase,
-//             useExisting: QuickSelectComponent
-//         }]
-// })
-// export class QuickSelectComponent extends ComponentBase implements OnInit, AfterViewInit {
-//
-//     constructor(private quick_table: QuickTableComponent) {
-//         super();
-//     }
-//
-//     @Input()
-//     public source: any[];
-//
-//     @Input('name-selector')
-//     public name_selector: string = 'name';
-//
-//     @Input('value')
-//     public default_value: string;
-//
-//     @Input('disabled')
-//     public is_disabled: boolean = false;
-//
-//     @Output('item-selected')
-//     public item_selected: EventEmitter<any> = new EventEmitter<any>();
-//
-//     @ContentChildren(QuickSelectItemDirective)
-//     public item_list: QueryList<QuickSelectItemDirective>;
-//
-//     public selected_item: QuickSelectItemDirective;
-//
-//     private changed() {
-//         this.quick_table.current_page = 1;
-//         this.quick_table.getIndexNumberList(this.quick_table.current_page);
-//         this.item_selected.emit(this.selected_item);
-//         this.onChange(this.selected_item);
-//     }
-//
-//     ngOnInit() {
-//         this.selected_item = null;
-//     }
-//
-//     ngAfterViewInit() {
-//         this.item_list.forEach(item => {
-//             if (item.selected) {
-//                 setTimeout(() => {
-//                     this.selected_item = item;
-//                     this.changed();
-//                 }, 2000);
-//             }
-//         });
-//     }
-// }
-
 import {AfterViewInit, Component, ContentChildren, EventEmitter, Input, OnInit, Output, QueryList} from '@angular/core';
 import {ComponentBase} from '../base.component';
 import {QuickSelectItemDirective} from './quick-select-item.directive';
@@ -77,7 +13,7 @@ import {el} from "@angular/platform-browser/testing/src/browser_util";
             useExisting: QuickSelectComponent
         }]
 })
-export class QuickSelectComponent extends ComponentBase implements OnInit {
+export class QuickSelectComponent extends ComponentBase implements OnInit, AfterViewInit {
 
     constructor(private quick_table: QuickTableComponent) {
         super();
@@ -112,5 +48,15 @@ export class QuickSelectComponent extends ComponentBase implements OnInit {
 
     ngOnInit() {
         this.selected_item = null;
+    }
+
+    ngAfterViewInit() {
+        this.item_list.forEach(item => {
+            if (item.selected) {
+                setTimeout(() => {
+                    this.selected_item = item;
+                }, 0);
+            }
+        });
     }
 }
