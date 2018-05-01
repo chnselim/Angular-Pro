bf421c4f89831cef14831eef8dc739484eba8ba0
chnselim <chnselim@outlook.com>
2018-05-01 13:12:51 +0300

    FIX selected source option when refresh the page.

--- components/quick-select/quick-select.component.ts
+++ components/quick-select/quick-select.component.ts
@@ -1,4 +1,4 @@
-import {Component, ContentChildren, EventEmitter, Input, OnInit, Output, QueryList} from '@angular/core';
+import {Component, ContentChildren, EventEmitter, Input, Output, QueryList, AfterViewInit} from '@angular/core';
 import {ComponentBase} from '../base.component';
 import {QuickSelectItemDirective} from './quick-select-item.directive';
 import {QuickTableComponent} from '../quick-table/quick-table.component';
@@ -12,7 +12,7 @@ import {QuickTableComponent} from '../quick-table/quick-table.component';
             useExisting: QuickSelectComponent
         }]
 })
-export class QuickSelectComponent extends ComponentBase {
+export class QuickSelectComponent extends ComponentBase implements AfterViewInit {
 
     constructor(private quick_table: QuickTableComponent) {
         super();
@@ -23,6 +23,9 @@ export class QuickSelectComponent extends ComponentBase {
 
     @Input('name-selector')
     public name_selector: string = 'name';
+
+    @Input('value-selector')
+    public value_selector: string = 'id';
 
     @Input('value')
     public default_value: string;
@@ -44,5 +47,24 @@ export class QuickSelectComponent extends ComponentBase {
         this.quick_table.getIndexNumberList(this.quick_table.current_page);
         this.item_selected.emit(this.selected_item);
         this.onChange(this.selected_item);
+    }
+
+    ngAfterViewInit() {
+        this.quick_table.query_parameters.forEach((value, key) => {
+            this.item_list.forEach(item => {
+                if (value === item.value) {
+                    this.selected_item = item;
+                }
+            });
+            setTimeout(() => {
+                if (this.source) {
+                    this.source.forEach(source_item => {
+                        if (value === source_item[this.value_selector]) {
+                            this.selected_item = source_item;
+                        }
+                    });
+                }
+            }, 1000);
+        });
     }
 }
