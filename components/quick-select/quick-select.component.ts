76fd0f684d3b30a0db073da579c981120c05d0c1
chnselim <chnselim@outlook.com>
2018-05-02 09:55:08 +0300

    ADD route listener to detect route changes and change selected filter value

--- components/quick-select/quick-select.component.ts
+++ components/quick-select/quick-select.component.ts
@@ -2,6 +2,7 @@ import {Component, ContentChildren, EventEmitter, Input, Output, QueryList, Afte
 import {ComponentBase} from '../base.component';
 import {QuickSelectItemDirective} from './quick-select-item.directive';
 import {QuickTableComponent} from '../quick-table/quick-table.component';
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 
 @Component({
     selector: 'quick-select',
@@ -14,8 +15,17 @@ import {QuickTableComponent} from '../quick-table/quick-table.component';
 })
 export class QuickSelectComponent extends ComponentBase implements AfterViewInit {
 
-    constructor(private quick_table: QuickTableComponent) {
+    constructor(private quick_table: QuickTableComponent,
+                private route: ActivatedRoute,
+                private router: Router) {
         super();
+        this.router.events.forEach((event) => {
+            if (event instanceof NavigationEnd) {
+                this.selected_item = null;
+                this.setSelectedFilters();
+                this.setSelectedSourceFilters();
+            }
+        });
     }
 
     @Input()
@@ -42,6 +52,9 @@ export class QuickSelectComponent extends ComponentBase implements AfterViewInit
     @Input('selected-item')
     public selected_item: QuickSelectItemDirective;
 
+    @Input()
+    public key: string;
+
     private changed() {
         this.quick_table.current_page = 1;
         this.quick_table.getIndexNumberList(this.quick_table.current_page);
@@ -50,21 +63,44 @@ export class QuickSelectComponent extends ComponentBase implements AfterViewInit
     }
 
     ngAfterViewInit() {
+        this.selected_item = null;
+        this.setSelectedFilters();
+        setTimeout(() => {
+            this.setSelectedSourceFilters();
+        }, 1000);
+    }
+
+    private setSelectedFilters() {
         this.quick_table.query_parameters.forEach((value, key) => {
             this.item_list.forEach(item => {
-                if (value === item.value) {
-                    this.selected_item = item;
+                if (this.key) {
+                    if (value === item.value && key === this.key) {
+                        this.selected_item = item;
+                    }
+                } else if (!this.key) {
+                    if (value === item.value) {
+                        this.selected_item = item;
+                    }
                 }
             });
-            setTimeout(() => {
-                if (this.source) {
-                    this.source.forEach(source_item => {
+        });
+    }
+
+    private setSelectedSourceFilters() {
+        this.quick_table.query_parameters.forEach((value, key) => {
+            if (this.source) {
+                this.source.forEach(source_item => {
+                    if (this.key) {
+                        if (value === source_item[this.value_selector] && key === this.key) {
+                            this.selected_item = source_item;
+                        }
+                    } else if (!this.key) {
                         if (value === source_item[this.value_selector]) {
                             this.selected_item = source_item;
                         }
-                    });
-                }
-            }, 1000);
+                    }
+                });
+            }
         });
     }
 }
