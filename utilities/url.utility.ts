e0da9cdebf6a35f1a43d13244004a3ff3ac3637a
chnselim <chnselim@outlook.com>
2018-05-09 16:03:22 +0300

    FIX component via Angular v6.0.0

--- utilities/url.utility.ts
+++ utilities/url.utility.ts
@@ -1,9 +1,8 @@
-import {URLSearchParams} from '@angular/http';
 import {HttpParams} from '@angular/common/http';
 
 export class UrlUtility {
     public static buildURLSearchParams(data: Map<string, string>): HttpParams {
-        var params = new HttpParams();
+        let params = new HttpParams();
         if (data) {
             data.forEach((value, key) => {
                 params = params.append(key, value);
